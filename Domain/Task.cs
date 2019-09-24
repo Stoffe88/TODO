6da88def3278518100f7e19b9fcd81eda7cf47a9
Kristoffer <kristoffer.kallerup@outlook.com>
2019-09-24 10:54:34 +0200

    Add ability to add todo

---
+++ Domain/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO.Domain
+{
+    class Task
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool TaskDone { get; set; }
+
+
+        public Task(string title, DateTime dueDate)
+        {
+            Title = title;
+            DueDate = dueDate;
+            TaskDone = false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TODO.Domain;
 using static System.Console;
 
 namespace TODO
@@ -7,6 +8,8 @@ namespace TODO
     {
         static void Main(string[] args)
         {
+            Task[] taskList = new Task[2];
+
             bool isRunning = true;
 
             while (isRunning)
@@ -19,9 +22,30 @@ namespace TODO
 
                 var pressedKey = ReadKey(true);
 
+                Clear();
+
                 switch (pressedKey.Key)
                 {
                     case ConsoleKey.D1:
+
+                        Write("Title: ");
+                        var title = ReadLine();
+
+                        Write("Due date (yyyy-MM-dd hh:mm): ");
+                        var dueDate = DateTime.Parse(ReadLine());
+
+                        Task task = new Task(title, dueDate);
+                        
+                        if (TryFindAvailablePosition(taskList, out int index))
+                        {
+                            taskList[index] = task;
+                        }
+                        else
+                        {
+                            Clear();
+                            Console.WriteLine("Task could not be added because you have too many tasks to do!");
+                            System.Threading.Thread.Sleep(2000);
+                        }
 
                         break;
 
@@ -41,7 +65,26 @@ namespace TODO
                 }
             }
 
+            
 
+
+        }
+        public static bool TryFindAvailablePosition(Task[] tasks, out int index)
+        {
+            index = 0;
+
+            while (index < tasks.Length && tasks[index] != null)
+            {
+                index++;
+            }
+
+
+            if (index < tasks.Length && tasks[index] == null)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
