a68b38e94792dc1872fb944920987e4ee9670cd1
Kristoffer <kristoffer.kallerup@outlook.com>
2019-09-24 09:38:51 +0200

    Add menu and exit ability

--- Program.cs
+++ Program.cs
@@ -7,7 +7,41 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Clear();
+
+                WriteLine("1. Add todo");
+                Console.WriteLine("2. List todos");
+                Console.WriteLine("3. Exit");
+
+                var pressedKey = ReadKey(true);
+
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+
+                        isRunning = false;
+
+                        break;
+
+                    default:
+
+                        break;
+                }
+            }
+
+
         }
     }
 }
